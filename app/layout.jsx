dc19d3b11e2e7ff25210937906b4f66bef0a4295
Crypto Titan <146081748+Cryptotytan@users.noreply.github.com>
2026-03-07 21:33:52 +0100

    Delete app directory

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <html>
-      <body style={{ margin: 0, padding: 0, background: "#07090e" }}>
-        {children}
-      </body>
-    </html>
-  );
-}
