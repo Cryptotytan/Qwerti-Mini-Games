5ee340060d7533c82f6a83c8d2388d6c817c20bc
Crypto Titan <146081748+Cryptotytan@users.noreply.github.com>
2026-03-07 19:33:52 +0100

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,9 @@
+export default function Layout({ children }) {
+  return (
+    <html>
+      <body style={{ margin: 0, padding: 0, background: "#07090e" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
